5694f67ca29850b18e37cb8e656cd78b0f6ef554
hYdos <haydenv06@gmail.com>
2020-07-17 10:50:26 +1000

    ramid help

--- src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/server/MinecraftServerMixin.java
+++ src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/server/MinecraftServerMixin.java
@@ -1,8 +1,10 @@
 package io.github.fukkitmc.fukkit.mixins.net.minecraft.server;
 
 import net.minecraft.server.MinecraftServer;
+import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.Main;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,9 +12,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
 
+    @Shadow public CraftServer server;
+
     @Inject(method = "main", at = @At("HEAD"))
     private static void mainMethod(String[] args, CallbackInfo ci) {
         Main.main(args);
     }
 
+    @Inject(method = "method_6521", at = @At("TAIL"))
+    public void postWorldLoadPlugins(CallbackInfo ci){
+        this.server.enablePlugins(org.bukkit.plugin.PluginLoadOrder.POSTWORLD); // CraftBukkit
+    }
 }
--- src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/server/dedicated/DedicatedServerMixin.java
+++ src/main/java/io/github/fukkitmc/fukkit/mixins/net/minecraft/server/dedicated/DedicatedServerMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.server.dedicated.DedicatedPlayerManager;
 import net.minecraft.server.dedicated.DedicatedServer;
 import org.bukkit.craftbukkit.Main;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,16 +20,29 @@ import java.net.Proxy;
 @Mixin(DedicatedServer.class)
 public abstract class DedicatedServerMixin extends MinecraftServer {
 
+    @Shadow
+    public abstract DedicatedPlayerManager getPlayerManager();
+
     public DedicatedServerMixin(File gameDir, Proxy proxy, File file) {
         super(gameDir, proxy, file);
     }
 
-    @Inject(method = "setupServer", at = @At("TAIL"))
-    public void initSpigotConfig(CallbackInfoReturnable<Boolean> cir){
+    @Inject(method = "setupServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/dedicated/DedicatedServer;isOnlineMode()Z", shift = At.Shift.BEFORE))
+    public void spigotLoadPlugins(CallbackInfoReturnable<Boolean> cir) {
         // Fukkit start
         org.spigotmc.SpigotConfig.init((File) Main.options.valueOf("spigot-settings"));
         org.spigotmc.SpigotConfig.registerCommands();
         // Fukkit end
+        this.playerManager = (new DedicatedPlayerManager((DedicatedServer) (Object) this)); // Fukkit
+        server.loadPlugins();
+        server.enablePlugins(org.bukkit.plugin.PluginLoadOrder.STARTUP);
+        // Spigot End
     }
 
+    @Inject(method = "setupServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/dedicated/DedicatedServer;setPlayerManager(Lnet/minecraft/server/PlayerManager;)V"))
+    public void cancelPlayerManagerSetup(CallbackInfoReturnable<Boolean> cir) {
+        if (getPlayerManager() == null) {
+            this.setPlayerManager(new DedicatedPlayerManager((DedicatedServer) (Object) this)); // Fukkit
+        }
+    }
 }
